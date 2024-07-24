d2c7415f54cb46a09eb939e6d735c283abcf8300
mtnlmptnss <mtnlmptnss@gmail.com>
2024-07-24 11:57:39 +0300

    Css lan

---
+++ Bleachigo.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleMenuButton = document.querySelector('.toggle-menu');
+    const menu = document.querySelector('.menu');
+
+    toggleMenuButton.addEventListener('click', function() {
+        menu.classList.toggle('visible');
+    });
+});
